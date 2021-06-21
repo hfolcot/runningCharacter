ed97c5785cb85768b1d308daab49ac44d6261c16
Heather Olcot <holcot@505games.com>
2021-06-21 16:44:44 +0100

    mobile friendly tweaks

--- js/script.js
+++ js/script.js
@@ -9,7 +9,8 @@ const scroller = new LocomotiveScroll({
     },
     smartphone: {
         direction: 'horizontal',
-        smooth: true
+        smooth: true,
+        gestureDirection: 'both',
     }
 });
 
@@ -19,7 +20,7 @@ const character = document.getElementById('char');
 scroller.on('scroll', (e) => {
     //console.log(e);
     if (e.scroll.x > 0.5 && e.delta.x < e.limit.x) {
-
+        document.querySelector('.msh-container').classList.add('fadeOut');
         window.clearTimeout(scrolling);
         if (e.direction === 'left') {
             character.classList.add('left');

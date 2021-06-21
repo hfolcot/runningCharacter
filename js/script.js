38f9554636e5572771222b0dffb4d2d064735744
Heather Olcot <holcot@505games.com>
2021-06-21 12:20:24 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    direction: 'horizontal'
+});
+
+const character = document.getElementById('char');
+const scrollContainer = document.querySelector('[data-scroll-container]');
+
+let scrolling = false;
+let scrollDirection = 'd';
+
+document.addEventListener('wheel', function (e) {
+    character.classList.add('run');
+    character.classList.remove('idle');
+    window.clearTimeout(scrolling);
+    if (e.deltaY <= 0) {
+        scrollDirection = 'u';
+    } else {
+        scrollDirection = 'd';
+    }
+    if (scrollDirection === 'u') {
+        character.classList.add('left');
+    } else {
+        character.classList.remove('left');
+    }
+
+    scrolling = window.setTimeout(() => {
+        character.classList.add('idle');
+        character.classList.remove('run');
+    }, 500)
+})
